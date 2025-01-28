646ce994715291bbf2cdf45b3a32c0c7b51ce802
Callan <157165317+C-garner010@users.noreply.github.com>
2025-01-27 19:32:05 -0700

    Add files via upload
    Week 2, cheers!

---
+++ HW2/script.js
@@ -0,0 +1,38 @@
+console.log("hello world!");
+
+// single line comment
+
+/* 
+Multi-line comments
+would go here
+and here
+here again
+etc
+*/
+
+// 3 favorite websites 
+console.log("Favorite Websites:");
+console.log("1. https://www.dailyom.com/ - Not only do they have interesting articles, but they also share courses more personal in nature.");
+console.log("2. https://www.highexistence.com/ - A beautifully crafted rabbit hole, one of my favorite places to spend time in.");
+console.log("3. https://www.google.com/maps - I love snooping around different areas in satellite mode. I have found some interesting places for sure!");
+
+// 3 favorite games
+console.log("Favorite Games:");
+console.log("1. Sudoku - A brilliant little puzzle that can transcend reality at times.");
+console.log("2. GeoGuesser - Similar to perusing Google Maps, but trying to figure out where you've landed instead (the closer you are, the more points you get)! ");
+console.log("3. The Room - Another puzzle game, you're asked to figure out a mysterious and beautiful 3D puzzle.");
+
+// 3 favorite artists and their cultural importance 
+console.log("Favorite Artists:");
+console.log("1. Jon Batiste - From soul, jazz, a bit of rap and pop, to Beethoven Blues, he is quite the phenomenal musician.");
+console.log("2. Leon Bridges - Another phenomenal artist - pulling motivation from Sam Cooke and Otis Redding - Bridges has helped to bring soul back.");
+console.log("3. Labrinth - Known for blending pop, gospel, electronic, and hip-hop, Labrinth seems genius in his innovations.");
+
+// name question using prompt and console.log
+// do not need to verify if correct 
+const name = prompt("What is your name?");
+console.log(`Hello, ${name}! Nice to meet you.`)
+
+// practicing quotes vs backticks
+const artist = prompt('Who is your favorite artist?!');
+console.log("Super neat! " + artist + " sounds interesting, I will have to check them out!");
